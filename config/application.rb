4163fde2be93a18e0f63284c4923d430f627472c
William Weckl <william.weckl@gmail.com>
2016-03-11 17:42:05 -0300

    pt-BR translate.

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,14 @@ module Segmentation
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    #Encoding
+    config.encoding = "utf-8"
+
+    #I18n config
+    config.i18n.enforce_available_locales = true
+    config.i18n.available_locales = [:"pt-BR"]
+    config.i18n.default_locale = :"pt-BR"
+    config.i18n.locale = :"pt-BR"
   end
 end
