5e069bae8dea3e551c46b848bb71108bb69a87e7
William Weckl <william.weckl@gmail.com>
2016-03-11 23:07:08 -0300

    Creating example positions and contacts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+50.times do |n|
+  Position.find_or_create_by!(name: Faker::Company.profession)
+end
+
+100.times do |n|
+  name = Faker::Name.name
+  contact = Contact.create(name: name, email: Faker::Internet.email(name), state: State.list.sample, age: (17..80).to_a.sample, position: Position.all.sample)
+end
