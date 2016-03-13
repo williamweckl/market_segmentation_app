ec1fbdc6137bb84883e8dc3554efcea67c1fced6
William Weckl <william.weckl@gmail.com>
2016-03-13 00:25:55 -0300

    AppCtrl unit tests.

--- app/assets/javascripts/controllers/AppCtrl.js
+++ app/assets/javascripts/controllers/AppCtrl.js
@@ -1,15 +1,11 @@
 this.app.controller('AppCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
     //Toggle filters sidenav
     $scope.toggleFilters = function() {
-        console.log('toggleFilters');
-
         $mdSidenav('filters')
             .toggle();
     };
     //Close filters sidenav
     $scope.closeFilters = function() {
-        console.log('close');
-
         $mdSidenav('filters')
             .close();
     };
